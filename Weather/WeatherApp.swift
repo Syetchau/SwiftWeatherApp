23b754a9aa2f321d67b2076045260a6219dffcdd
syetchau95 <syetchau@hotmail.com>
2022-01-05 20:35:37 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by LiewSyetChau on 5/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
